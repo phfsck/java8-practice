b37001edd8bab66e089bdd5042a902e8307266a6
phfsck <coeus.ph@gmail.com>
2019-09-29 15:52:04 +0800

    Initial commit.

---
+++ src/main/java/com/jarv/practice/Java8PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jarv.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8PracticeApplication.class, args);
+	}
+
+}
