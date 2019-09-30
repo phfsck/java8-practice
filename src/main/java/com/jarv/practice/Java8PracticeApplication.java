2f6fee13ca77ab0259c13af8be9a28f9ffe99afc
Jarvy  Alvarez <j.alvarez@ph.fujitsu.com>
2019-09-30 17:49:21 +0800

    Log4J2 implementation.

--- src/main/java/com/jarv/practice/Java8PracticeApplication.java
+++ src/main/java/com/jarv/practice/Java8PracticeApplication.java
@@ -1,7 +1,6 @@
 package com.jarv.practice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +9,6 @@ import com.jarv.practice.app.Java8Processor;
 
 @SpringBootApplication
 public class Java8PracticeApplication implements CommandLineRunner {
-	
-	@Value("${app.config.name}")
-	private String name;
 	
 	@Autowired
 	Java8Processor j8Proc;
@@ -24,7 +20,6 @@ public class Java8PracticeApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		j8Proc.execute();
-		System.out.println("NAME: " + name);
 	}
 
 }
